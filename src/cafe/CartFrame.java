991a62c55d836767dd98058a61826e8f97c70589
trandinhvu2304 <mat0dich@gmail.com>
2024-12-07 22:45:13 +0700

    update

--- src/cafe/CartFrame.java
+++ src/cafe/CartFrame.java
@@ -26,9 +26,7 @@ public class CartFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton2 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
@@ -39,31 +37,22 @@ public class CartFrame extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         jTextField3 = new javax.swing.JTextField();
         jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
         jTextField6 = new javax.swing.JTextField();
         jTextField7 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
         jTextField8 = new javax.swing.JTextField();
-        jTextField9 = new javax.swing.JTextField();
-        jButton4 = new javax.swing.JButton();
-
-        jButton2.setBackground(new java.awt.Color(237, 226, 219));
-        jButton2.setFont(new java.awt.Font("Times New Roman", 1, 20)); // NOI18N
-        jButton2.setText("payment");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel2.setBackground(new java.awt.Color(158, 111, 78));
-        jPanel2.setLayout(null);
+        jPanel1.setBackground(new java.awt.Color(158, 111, 78));
+        jPanel1.setLayout(null);
 
         jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 30)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
@@ -74,8 +63,8 @@ public class CartFrame extends javax.swing.JFrame {
                 jLabel4MouseClicked(evt);
             }
         });
-        jPanel2.add(jLabel4);
-        jLabel4.setBounds(866, 6, 28, 28);
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(851, 6, 28, 28);
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -87,14 +76,14 @@ public class CartFrame extends javax.swing.JFrame {
         ));
         jScrollPane2.setViewportView(jTable2);
 
-        jPanel2.add(jScrollPane2);
-        jScrollPane2.setBounds(314, 50, 570, 460);
+        jPanel1.add(jScrollPane2);
+        jScrollPane2.setBounds(260, 80, 624, 440);
 
         jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Cash ($):");
-        jPanel2.add(jLabel2);
-        jLabel2.setBounds(30, 450, 120, 20);
+        jLabel2.setText("Change ($):");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(30, 530, 100, 20);
 
         jTextField1.setFont(new java.awt.Font("Times New Roman", 0, 16)); // NOI18N
         jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -104,125 +93,118 @@ public class CartFrame extends javax.swing.JFrame {
                 jTextField1ActionPerformed(evt);
             }
         });
-        jPanel2.add(jTextField1);
-        jTextField1.setBounds(30, 480, 240, 30);
+        jPanel1.add(jTextField1);
+        jTextField1.setBounds(30, 560, 207, 30);
 
         jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("Payment Date:");
-        jPanel2.add(jLabel3);
-        jLabel3.setBounds(30, 90, 100, 20);
+        jLabel3.setText("Tax ($):");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(30, 190, 100, 20);
 
         jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jPanel2.add(jLabel5);
-        jLabel5.setBounds(21, 448, 0, 0);
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(21, 442, 0, 0);
 
         jLabel6.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setText("Tax ($):");
-        jPanel2.add(jLabel6);
-        jLabel6.setBounds(30, 170, 63, 20);
+        jLabel6.setText("Customer Name:");
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(30, 390, 130, 20);
 
         jTextField3.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.add(jTextField3);
-        jTextField3.setBounds(30, 200, 240, 30);
+        jPanel1.add(jTextField3);
+        jTextField3.setBounds(30, 350, 207, 30);
 
         jTextField4.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.add(jTextField4);
-        jTextField4.setBounds(30, 120, 240, 30);
+        jPanel1.add(jTextField4);
+        jTextField4.setBounds(30, 220, 207, 30);
 
-        jTextField5.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.add(jTextField5);
-        jTextField5.setBounds(30, 270, 240, 30);
+        jButton2.setBackground(new java.awt.Color(237, 226, 219));
+        jButton2.setFont(new java.awt.Font("Times New Roman", 1, 20)); // NOI18N
+        jButton2.setText("Payment");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2);
+        jButton2.setBounds(260, 560, 620, 30);
 
         jLabel7.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 255, 255));
         jLabel7.setText("Payment ID:");
-        jPanel2.add(jLabel7);
-        jLabel7.setBounds(30, 20, 120, 20);
+        jPanel1.add(jLabel7);
+        jLabel7.setBounds(30, 50, 100, 20);
 
         jLabel8.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel8.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel8.setText("Sub Total ($):");
-        jPanel2.add(jLabel8);
-        jLabel8.setBounds(30, 240, 120, 20);
+        jLabel8.setText("Payment Date:");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(30, 120, 110, 20);
 
         jLabel9.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel9.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel9.setText("Total ($):");
-        jPanel2.add(jLabel9);
-        jLabel9.setBounds(30, 310, 120, 20);
+        jLabel9.setText("Sub Total ($):");
+        jPanel1.add(jLabel9);
+        jLabel9.setBounds(30, 260, 110, 20);
 
         jLabel10.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel10.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel10.setText("Customer Name:");
-        jPanel2.add(jLabel10);
-        jLabel10.setBounds(30, 380, 120, 20);
+        jLabel10.setText("Total ($):");
+        jPanel1.add(jLabel10);
+        jLabel10.setBounds(30, 320, 90, 20);
 
         jLabel11.setFont(new java.awt.Font("Times New Roman", 1, 16)); // NOI18N
         jLabel11.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel11.setText("Change ($):");
-        jPanel2.add(jLabel11);
-        jLabel11.setBounds(30, 520, 120, 20);
+        jLabel11.setText("Cash ($):");
+        jPanel1.add(jLabel11);
+        jLabel11.setBounds(30, 460, 90, 20);
+
+        jTextField5.setBackground(new java.awt.Color(204, 204, 204));
+        jPanel1.add(jTextField5);
+        jTextField5.setBounds(30, 150, 207, 30);
 
         jTextField6.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.add(jTextField6);
-        jTextField6.setBounds(30, 50, 240, 30);
+        jPanel1.add(jTextField6);
+        jTextField6.setBounds(30, 80, 207, 30);
 
         jTextField7.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.add(jTextField7);
-        jTextField7.setBounds(30, 340, 240, 30);
+        jPanel1.add(jTextField7);
+        jTextField7.setBounds(30, 290, 207, 30);
 
-        jTextField8.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.add(jTextField8);
-        jTextField8.setBounds(30, 550, 240, 30);
-
-        jTextField9.setFont(new java.awt.Font("Times New Roman", 0, 16)); // NOI18N
-        jTextField9.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextField9.setText("0");
-        jTextField9.addActionListener(new java.awt.event.ActionListener() {
+        jTextField2.setFont(new java.awt.Font("Times New Roman", 0, 16)); // NOI18N
+        jTextField2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField2.setText("0");
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField9ActionPerformed(evt);
+                jTextField2ActionPerformed(evt);
             }
         });
-        jPanel2.add(jTextField9);
-        jTextField9.setBounds(30, 410, 240, 30);
+        jPanel1.add(jTextField2);
+        jTextField2.setBounds(30, 420, 207, 30);
 
-        jButton4.setBackground(new java.awt.Color(204, 204, 255));
-        jButton4.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
-        jButton4.setText("Payment");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        jTextField8.setFont(new java.awt.Font("Times New Roman", 0, 16)); // NOI18N
+        jTextField8.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField8.setText("0");
+        jTextField8.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                jTextField8ActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton4);
-        jButton4.setBounds(310, 550, 570, 30);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
+        jPanel1.add(jTextField8);
+        jTextField8.setBounds(30, 490, 207, 30);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 900, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
         );
 
         pack();
@@ -241,13 +223,13 @@ public class CartFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jTextField9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField9ActionPerformed
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField9ActionPerformed
+    }//GEN-LAST:event_jTextField2ActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void jTextField8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField8ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_jTextField8ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -286,7 +268,6 @@ public class CartFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
@@ -298,16 +279,15 @@ public class CartFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
     private javax.swing.JTextField jTextField5;
     private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField8;
-    private javax.swing.JTextField jTextField9;
     // End of variables declaration//GEN-END:variables
 }
